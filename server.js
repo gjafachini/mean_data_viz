eae51196d86445ed14361974c1c33bdc6ebe1567
Guilherme Fachini <guilherme.fachini@gmail.com>
2015-11-21 14:24:09 -0200

    Fixing packages

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoconnect.setup('mongodb://kiva_view:12345Kiva@ds047524.mongolab.com:47524/ki
 
 // Logging and Parsing
 app.use(express.static(__dirname + '/public'));                 // sets the static files location to public
-app.use('/bower_components',  express.static(__dirname + '/bower_components')); // Use BowerComponents
+app.use('/node_modules',  express.static(__dirname + '/node_modules')); // Use node_modules
 app.use(morgan('dev'));                                         // log with Morgan
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
