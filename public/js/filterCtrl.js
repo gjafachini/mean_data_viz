953fed3e3ed89eea5f9ac209c32744059e71f9fb
Guilherme Fachini <guilherme.fachini@gmail.com>
2015-11-22 15:40:44 -0200

    Fixing map and table

--- public/js/filterCtrl.js
+++ public/js/filterCtrl.js
@@ -1,6 +1,6 @@
-var filterCtrl = angular.module('filterCtrl', ['checklist-model', 'googlechart', 'ngTable']);
+var filterCtrl = angular.module('filterCtrl', ['checklist-model', 'googlechart']);
 google.load('visualization', '1', { packages: ['Geochart', 'table'] });
-filterCtrl.controller('filterCtrl', function($filter, $scope, $http, ngTableParams){
+filterCtrl.controller('filterCtrl', function($filter, $scope, $http){
 
     $scope.sectors = ['Agriculture', 
                       'Arts', 
@@ -44,59 +44,59 @@ filterCtrl.controller('filterCtrl', function($filter, $scope, $http, ngTablePara
         $scope.filter.sectors = [];
     };
 
-    $scope.updateMaps = function(queryResults) {
-        var chart1 = {};
-        chart1.type = "GeoChart";
+    $scope.updateMaps = function(queryResult) {
 
-        chart1.data = [['Country', 'Debt']];
-        for(var i=0; i<queryResults.length; i++) {
-            if (queryResults[i].debt != null) {
-                chart1.data[i+1] = [queryResults[i].country, queryResults[i].debt];
-                console.log(chart1.data[i+1]);                
+        var options = {
+            //legend:'null',
+            colorAxis: {colors: ['#e31b23', 'white', '#4374e0']},
+            //sizeAxis: {minValue: 0, maxSize: 50},
+            backgroundColor: '#81d4fa'
+        }; 
+
+        var map_data = new google.visualization.DataTable();
+        map_data.addColumn('string', 'Country');
+        map_data.addColumn('number', 'Debt');
+
+        for(var i=0; i<queryResult.length; i++) {
+            if (queryResult[i].debt != null) {
+                if(queryResult[i].country == 'US') {
+                    map_data.addRow([queryResult[i].country, 600000]);
+                } else {
+                    map_data.addRow([queryResult[i].country, Number(queryResult[i].debt)]);
+                }
             }
         }
 
-        chart1.options = {
-            chartArea: {left:10,top:10,bottom:0,height:"100%"},
-            colorAxis: {colors: ['red', 'green']},
-            displayMode: 'regions',
-            backgroundColor: '#81d4fa'
-        };
-
-        chart1.formatters = {
-            number : [{columnNum: 1, pattern: "$ #,##0.00"}]
-        };
-
-        $scope.chart = chart1;
+        $scope.Geochart = new google.visualization.GeoChart(document.getElementById('Geochart'));
+        $scope.Geochart.draw(map_data, options);
     }
 
     $scope.queryRanks = function() {
-
+        console.log("Filter called: " + $scope.filter);
         // Post the queryRanks to the /queryRanks POST route to retrieve the filtered results
         $http.post('/queryRanks', $scope.filter)
             // Store the filtered results in queryResults
             .success(function(queryResult){
                 // Pass the filtered results to the Google Map Service and refresh the map
-                ///$scope.updateMaps(queryResult);
+                $scope.updateMaps(queryResult);
 
                 // Update table
                 console.log("Loading Table");
 
-                $scope.tableParams = new ngTableParams({
-                    page: 1,            // show first page
-                    total: queryResult.length, // length of data
-                    count: 10,           // count per page
-                    sorting: { debt: "desc" },
-                });
+                var query_data = new google.visualization.DataTable();
+                query_data.addColumn('string', 'Country');
+                query_data.addColumn('number', 'Debt');
+                query_data.addColumn('number', 'Lends');
+                query_data.addColumn('number', 'Loans');
 
-                $scope.$watch('tableParams', function(params) {
-                    console.log(params);
+                for(var i=0; i<queryResult.length; i++) {
+                    if (queryResult[i].debt != null) {
+                        query_data.addRow([queryResult[i].country, Number(queryResult[i].debt), Number(queryResult[i].lends), Number(queryResult[i].loans)]);
+                    }
+                }
 
-                    var orderedData = params.sorting ? $filter('orderBy')(queryResult, params.orderBy()) : queryResult;
-
-                    $scope.table_data = orderedData;
-                    console.log($scope.table_data);
-                }, true);
+                $scope.table = new google.visualization.Table(document.getElementById('table'));
+                $scope.table.draw(query_data, {width: '100%', height: '100%'});
             })
         
             .error(function(queryResult){
